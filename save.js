76d2300069b50b22fdcd1ca6e61ffedc825bdc0b
Milan Fourie <72185636+milanfourie@users.noreply.github.com>
2020-11-06 23:17:12 +0200

    Metadata Logic

--- save.js
+++ save.js
@@ -5,10 +5,10 @@ router.post('/', (req, res) => {
     let formData = {
         fileName: req.body.fileName,
         nameAndSurname: req.body.nameAndSurname.replace(' ', '') != '' ? true : false,
-        contactNumber: req.body.contactNumber.replace(' ', '') != '' : false,
-        emailAddress: req.body.emailAddress.replace(' ', '') != '' : false,
+        contactNumber: req.body.contactNumber.replace(' ', '') != '' ? true : false,
+        emailAddress: req.body.emailAddress.replace(' ', '') != '' ? true: false,
         idNumber: req.body.idNumber.replace(' ', '') != '' ? true : false,
-        dateOfBirth: req.body.dateOfBirth.replace(' ', '') != ' ' ? true : false,
+        dateOfBirth: req.body.dateOfBirth.replace(' ', '') != '' ? true : false,
         linkedInProfile: req.body.linkedInProfile.replace(' ', '') != ' ' ? true : false
     }
 
